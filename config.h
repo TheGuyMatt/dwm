3c1e374b0fdc18d711096d190346a36f153ee8ea
TheGuyMatt <matthew.hieb@protonmail.com>
2020-06-23 13:34:39 -0700

    Fixed lockscreen function. Reduced gap size

--- config.h
+++ config.h
@@ -5,8 +5,8 @@
 
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
-static const unsigned int gappx     = 20;        /* gaps between windows */
-static const unsigned int snap      = 10;       /* snap pixel */
+static const unsigned int gappx     = 10;        /* gaps between windows */
+static const unsigned int snap      = 5;       /* snap pixel */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
@@ -75,7 +75,7 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "rofi", "-show", "run", "-eh", "2", "-fullscreen", "-theme", "~/.cache/wal/colors-rofi-dark.rasi", "-font", "Hack Nerd Font 18", NULL };
 //static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
-static const char *lockscreen[]  = { "~/.local/bin/betterlockscreen", "-l", "blur", NULL };
+static const char *lockscreen[]  = { "betterlockscreen", "-l", "blur", NULL };
 
 //brightness functions
 static const char *brightnessup[] = { "/usr/bin/light", "-A", "5", NULL };
